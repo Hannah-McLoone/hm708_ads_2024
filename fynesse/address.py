86b7f489a6537f9ece543207982fb20ca90cc84b
Hannah-McLoone <133026393+Hannah-McLoone@users.noreply.github.com>
2024-12-07 17:43:54 +0000

    Update address.py

--- fynesse/address.py
+++ fynesse/address.py
@@ -23,6 +23,61 @@ from sklearn.linear_model import LinearRegression
 from matplotlib import pyplot as plt
 
 
+
+
+
+def k_fold(k, X, y, alpha=0, L1_wt=0, plot = False):
+  indexes =[i for i in range(len(X))]
+  X = X.astype(float)
+  y = y.astype(float)
+  random.shuffle(indexes)
+  X  = X.iloc[indexes]
+  y  = y.iloc[indexes]
+  start = 0
+  end = 0
+  predicted = []
+
+
+  remainder = len(indexes)%k
+  length = len(indexes)//k
+
+  for i in range (0,k):
+    end = end + length
+    if remainder > 0:
+      end = end + 1
+      remainder = remainder - 1
+
+    testing_design = X.iloc[start:end]
+    testing_y = y.iloc[start:end]
+    training_design = pd.concat([X.iloc[0:start], X.iloc[end:len(indexes)]])
+    training_y = pd.concat([y.iloc[0:start], y.iloc[end:len(indexes)]])
+
+    # Fit a simple linear model
+    m_linear_basis = sm.OLS(training_y, training_design)
+    results_basis = m_linear_basis.fit_regularized(alpha = alpha, L1_wt = L1_wt)
+
+    #uses a design matrix of non-linear functions.
+    #y_pred_linear_basis = results_basis.get_prediction(testing_design).summary_frame()
+    y_pred_linear_basis = results_basis.predict(testing_design)
+
+    predicted = predicted + list(y_pred_linear_basis)
+    start = end
+
+    if plot:
+      plt.scatter(testing_y, y_pred_linear_basis, alpha=0.1, s = 1)
+
+  plt.xlim(-1, 1)
+  plt.ylim(-1.5, 1.5)
+  if plot:
+    plt.show()
+
+  if isinstance(y, pd.DataFrame):
+    y = y.values.flatten().tolist()
+  correlation, p_value = pearsonr(y, predicted)
+  return(correlation, p_value)
+
+
+
 def k_fold(k, X, y, alpha=0, L1_wt=0, plot = False):
   indexes =[i for i in range(len(X))]
   X = X.astype(float)
